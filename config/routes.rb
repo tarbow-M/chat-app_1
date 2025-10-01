02262d7d2b7ea4817f0e6efb3471cb25f4d2db62
tarbow-M <2020shikata@gmail.com>
2025-10-01 23:09:17 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Userモデルに対してDeviseの全てのルーティングを定義する
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
