2689dc67af7f1abfab9510bbd0dacc40a40f7f5d
tarbow-M <2020shikata@gmail.com>
2025-10-01 14:19:58 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Userモデルに対してDeviseの全てのルーティングを定義する
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
