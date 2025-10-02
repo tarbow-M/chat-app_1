4be0db9fa17450f8990e96d491c52104d1e0e199
tarbow-M <2020shikata@gmail.com>
2025-10-02 18:12:10 +0900

    結合テストの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
       it "nameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
-        expect(@user).to be valid
+        expect(@user).to be_valid
 
       end
     end
